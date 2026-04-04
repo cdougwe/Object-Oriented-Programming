ecf0a750a2174e42bbba51d193aa4c458d4a31e3
李祐丞 <13361856@me.mcu.edu.tw>
2026-04-04 19:43:08 +0800

    3-2 source code

--- 3. 工廠方法/2/Main.java
+++ 3. 工廠方法/2/Main.java
@@ -1,5 +1,37 @@
+
+abstract class Sender {
+    Tool tool;
+
+    final void send(String msg) {
+        tool = genSender();
+        tool.sendMessage(msg);
+    }
+
+    abstract Tool genSender();
+}
+
+abstract class Tool {
+    abstract void sendMessage(String msg);
+}
+
+class Email extends Tool {
+    void sendMessage(String msg) {
+        System.out.println("發送 Email: " + msg);
+    }
+}
+
+class SMS extends Tool {
+    void sendMessage(String msg) {
+        System.out.println("發送 SMS: " + msg);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
+        Sender sender = new EmailSender();
+        sender.send("Hello");
+        sender = new SMSSender();
+        sender.send("Hello");
 
     }
 }
