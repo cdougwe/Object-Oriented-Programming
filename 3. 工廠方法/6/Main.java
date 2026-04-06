add3ca40ef94480e298c47820ee6395d60021594
李祐丞 <13361856@me.mcu.edu.tw>
2026-04-06 11:36:25 +0800

    3.6 ACCEPT / 100

--- 3. 工廠方法/6/Main.java
+++ 3. 工廠方法/6/Main.java
@@ -1,5 +1,78 @@
+enum CareerType {
+    MAGE, WARRIOR;
+}
+
+interface Role {
+    void setAttributes();
+    void equipWeapon();
+}
+
+class WarriorRole implements Role {
+    public void setAttributes() {
+        System.out.println("設置戰士的高攻擊與防禦");
+    }
+    public void equipWeapon() {
+        System.out.println("裝備劍");
+    }
+}
+
+class MageRole implements Role {
+    public void setAttributes() {
+        System.out.println("設置法師的高魔力");
+    }
+    public void equipWeapon() {
+        System.out.println("裝備魔杖");
+    }
+}
+
+abstract class Character {
+    Role role;
+
+    public final void initialize() {
+        role = genCharacter(); // 產生角色物件
+        role.setAttributes(); // 設定角色屬性
+        role.equipWeapon(); // 配置角色武器
+    }
+    abstract Role genCharacter();
+}
+
+class Warrior extends Character {
+    @Override
+    Role genCharacter() {
+        return new WarriorRole();
+    }
+}
+
+class Mage extends Character {
+    @Override
+    Role genCharacter() {
+        return new MageRole();
+    }
+}
+
+class CharacterFactory {
+    public Character getCareer(String p) {
+        if (p == null) {
+            throw new IllegalArgumentException("Career can not be a null type !");
+        }
+        try {
+            CareerType careerType = CareerType.valueOf(p.toUpperCase());
+            return switch (careerType) {
+                case MAGE -> new Mage();
+                case WARRIOR -> new Warrior();
+            };
+        } catch (Exception e) {
+            throw new IllegalArgumentException("This career is unacceptable !");
+        }
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-
+        CharacterFactory characterFactory = new CharacterFactory();
+        Character character = characterFactory.getCareer("Warrior");
+        character.initialize();
+        character = characterFactory.getCareer("MAGE");
+        character.initialize();
     }
 }
