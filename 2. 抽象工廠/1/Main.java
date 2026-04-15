135b20943e96c13cf636bf9d628c6df8ed39b5e7
李祐丞 <13361856@me.mcu.edu.tw>
2026-04-15 13:21:45 +0800

    2.1 ACCEPT / 100

--- 2. 抽象工廠/1/Main.java
+++ 2. 抽象工廠/1/Main.java
@@ -1,5 +1,56 @@
+interface Shape {
+    void draw();
+}
+
+class Circle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing a circle");
+    }
+}
+
+class Rectangle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing a rectangle");
+    }
+}
+
+interface ShapeFactory {
+    Shape createShape();
+}
+
+class RectangleFactory implements ShapeFactory {
+    @Override
+    public Shape createShape() {
+        return new Rectangle();
+    }
+}
+
+class CircleFactory implements ShapeFactory {
+    @Override
+    public Shape createShape() {
+        return new Circle();
+    }
+}
+
+class ShapeClient {
+    private Shape shape;
+
+    public ShapeClient(ShapeFactory shapeFactory) {
+        this.shape = shapeFactory.createShape();
+    }
+
+    public void drawShape() {
+        shape.draw();
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-
+        ShapeClient sh = new ShapeClient(new CircleFactory());
+        sh.drawShape();
+        sh = new ShapeClient(new RectangleFactory());
+        sh.drawShape();
     }
 }
