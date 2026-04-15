55217aa755b71d983347952af2a4345a2371a730
李祐丞 <13361856@me.mcu.edu.tw>
2026-04-15 19:08:49 +0800

    2.3 ACCEPT / 100

--- 2. 抽象工廠/3/Main.java
+++ 2. 抽象工廠/3/Main.java
@@ -1,5 +1,54 @@
+interface Chair {
+    void sitOn();
+}
+
+class GamingChair implements Chair {
+    public void sitOn() {
+        System.out.println("Sitting on Gaming Chair");
+    }
+}
+
+class OfficeChair implements Chair {
+    public void sitOn() {
+        System.out.println("Sitting on Office Chair");
+    }
+}
+
+interface ChairFactory {
+    Chair createChair();
+}
+
+class OfficeChairFactory implements ChairFactory {
+    @Override
+    public Chair createChair() {
+        return new OfficeChair();
+    }
+}
+
+class GamingChairFactory implements ChairFactory {
+    @Override
+    public Chair createChair() {
+        return new GamingChair();
+    }
+}
+
+class ChairClient {
+    private Chair chair;
+
+    public ChairClient(ChairFactory chairFactory) {
+        this.chair = chairFactory.createChair();
+    }
+
+    public void useChair() {
+        chair.sitOn();
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-
+        ChairClient os = new ChairClient(new OfficeChairFactory());
+        os.useChair();
+        os = new ChairClient(new GamingChairFactory());
+        os.useChair();
     }
 }
