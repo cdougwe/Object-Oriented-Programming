3931eebeb1c0f929385dc7f5aa7e9046eeb79c72
李祐丞 <13361856@me.mcu.edu.tw>
2026-04-15 15:00:31 +0800

    9.2 ACCEPT / 100

--- 9. Adapter模式/2/Main.java
+++ 9. Adapter模式/2/Main.java
@@ -1,25 +1,47 @@
-interface PaymentProcessor {
-    void pay(int amount);
+interface PaymentTarget {
+    void makePayment(double amount);
+}
+
+class OldPaymentProcessor {
+    private OldPaymentSystem oldPaymentSystem;
+
+    public OldPaymentProcessor(OldPaymentSystem oldPaymentSystem) {
+        this.oldPaymentSystem = oldPaymentSystem;
+    }
+    public void processPayment(double amount) {
+        oldPaymentSystem.makePayment(amount);
+    }
 }
 
 class OldPaymentSystem {
-    public void makePayment(int amount) {
+    public void makePayment(double amount) {
         System.out.println("Payment of " + amount + " made using old system");
     }
 }
 
-class OldPaymentAdapter implements PaymentProcessor {
+class OldPaymentAdapter implements PaymentTarget {
     private OldPaymentSystem oldPaymentSystem = new OldPaymentSystem();
 
     @Override
-    public void pay(int amount) {
+    public void makePayment(double amount) {
         oldPaymentSystem.makePayment(amount);
+    }
+}
+
+class PaymentProcessor {
+    private PaymentTarget target;
+
+    public PaymentProcessor(PaymentTarget target) {
+        this.target = target;
+    }
+    public void pay(double amount) {
+        target.makePayment(amount);
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        PaymentProcessor p = new OldPaymentAdapter();
-        p.pay(1000); // 顯示 "Payment of 1000 made using old system"。
+        PaymentProcessor p = new PaymentProcessor(new OldPaymentAdapter());
+        p.pay(10000);
     }
 }
