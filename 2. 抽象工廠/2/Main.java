9d7a1a0838c41719bc884eb5d88542f62ed53a3f
李祐丞 <13361856@me.mcu.edu.tw>
2026-04-15 13:58:50 +0800

    2.2 ACCEPT / 100

--- 2. 抽象工廠/2/Main.java
+++ 2. 抽象工廠/2/Main.java
@@ -1,5 +1,56 @@
+interface OperatingSystem {
+    void display();
+}
+
+class Windows10 implements OperatingSystem {
+    @Override
+    public void display() {
+        System.out.println("Displaying Windows 10");
+    }
+}
+
+class Windows11 implements OperatingSystem {
+    @Override
+    public void display() {
+        System.out.println("Displaying Windows 11");
+    }
+}
+
+interface OperatingSystemFactory {
+    OperatingSystem createWindows();
+}
+
+class Windows10Factory implements OperatingSystemFactory {
+    @Override
+    public OperatingSystem createWindows() {
+        return new Windows10();
+    }
+}
+
+class Windows11Factory implements OperatingSystemFactory {
+    @Override
+    public OperatingSystem createWindows() {
+        return new Windows11();
+    }
+}
+
+class OSClient {
+    private OperatingSystem operatingSystem;
+
+    public OSClient(OperatingSystemFactory operatingSystemFactory) {
+        this.operatingSystem = operatingSystemFactory.createWindows();
+    }
+
+    public void displayOperatingSystem() {
+        operatingSystem.display();
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-
+        OSClient os = new OSClient(new Windows10Factory());
+        os.displayOperatingSystem();
+        os = new OSClient(new Windows11Factory());
+        os.displayOperatingSystem();
     }
 }
