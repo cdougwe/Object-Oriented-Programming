ff161b5d67c7cc854980c690aa44a69c1fbebd3f
李祐丞 <snoopycucu42@gmail.com>
2026-04-17 17:00:37 +0800

    5.2 ACCEPT / 100

--- 5. Object Pool/2/Main.java
+++ 5. Object Pool/2/Main.java
@@ -1,5 +1,59 @@
+import java.util.LinkedList;
+
+class Logger {
+    public void log(String msg) {
+        System.out.println("Logging " + msg);
+    }
+}
+
+class LoggerPool {
+    private LinkedList<Logger> pool = new LinkedList<>();
+    private int maxSize;
+
+    public LoggerPool(int maxSize) {
+        this.maxSize = maxSize;
+        for (int i = 0; i < maxSize; i++) {
+            pool.add(new Logger());
+        }
+    }
+
+    public synchronized Logger getLogger() {
+        while (pool.isEmpty()) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        return pool.removeFirst();
+    }
+
+    public synchronized void releaseLogger(Logger logger) {
+        pool.addLast(logger);
+        notifyAll();
+    }
+}
+
+class LoggingService {
+    private LoggerPool pool;
+
+    public LoggingService(int size) {
+        pool = new LoggerPool(size);
+    }
+    public void logMessage(String message) {
+        Logger logger = pool.getLogger();
+        try {
+            logger.log(message);
+        } finally {
+            pool.releaseLogger(logger);
+        }
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-
+        LoggingService ls = new LoggingService(100);
+        ls.logMessage("Hello");
+        ls.logMessage("I am Tom");
     }
 }
