d97f7d9690328c44af6d377df9bc6dccfa9419df
李祐丞 <snoopycucu42@gmail.com>
2026-04-21 21:17:38 +0800

    9.3 ACCEPT / 100

--- 9. Adapter模式/3/Main.java
+++ 9. Adapter模式/3/Main.java
@@ -1,29 +1,37 @@
-interface DataProcessingSystem {
-    void processData(String file);
-}
-
 class LegacyFileReader {
     public void readFromFile(String filePath) {
         System.out.println("Reading from file: " + filePath);
     }
 }
 
-class LegacyAdapter implements DataProcessingSystem {
-    private LegacyFileReader legacyFileReader;
+interface DataProcessingTarget {
+    void processData(String file);
+}
 
-    public LegacyAdapter() {
-        this.legacyFileReader = new LegacyFileReader();
-    }
+class LegacyAdapter implements DataProcessingTarget {
+    private LegacyFileReader legacyFileReader = new LegacyFileReader();
 
     @Override
-    public void processData(String filePath) {
-        legacyFileReader.readFromFile(filePath);
+    public void processData(String file) {
+        legacyFileReader.readFromFile(file);
+    }
+}
+
+class DataProcessingSystem {
+    private DataProcessingTarget dataProcessingTarget;
+
+    public DataProcessingSystem(DataProcessingTarget dataProcessingTarget) {
+        this.dataProcessingTarget = dataProcessingTarget;
+    }
+
+    public void processData(String file) {
+        dataProcessingTarget.processData(file);
     }
 }
 
 class Main {
     public static void main(String[] args) {
-        DataProcessingSystem p = new LegacyAdapter();
+        DataProcessingSystem p = new DataProcessingSystem(new LegacyAdapter());
         p.processData("data.txt"); // 顯示 ""Reading from file: data.txt"。
     }
 }
